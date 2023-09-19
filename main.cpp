80f0bbfbcd4c591258d5322aebfd53c933d83c94
Richard Blažek <blazek.risa@gmail.com>
2023-09-19 14:28:27 +0200

    Quicksort works

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
+#include <iostream>
 #include "sort.hpp"
 
-#include <iostream>
 
 int main()
 {
     srand(time(0));
 
-    int items[4];
+    int items[40];
     for (int& item: items)
     {
         item = rand();
     }
 
-    quick_sort(items, items + 4);
+    quick_sort(items, items + 40);
     for (int item: items)
     {
         std::cout << item << "\n";
     }
 
     std::cout << "\n";
-    std::cout << "Steps: " << steps << "\n";
     return 0;
 }
--- sort.hpp
+++ sort.hpp
@@ -7,18 +7,6 @@
 using std::size_t;
 
 template<typename T>
-void dbg_print(T* begin, T* end)
-{
-    std::cout<<"[";
-    while (begin < end)
-    {
-        std::cout<<*begin<<" ";
-        ++begin;
-    }
-    std::cout<<"]\n";
-}
-
-template<typename T>
 void quick_sort(T* begin, T* end)
 {
     if (end <= begin + 1)
@@ -28,14 +16,26 @@ void quick_sort(T* begin, T* end)
     T* pivot = begin;// + rand() % (end - begin);
     for (T* ptr = begin; ptr < end; ++ptr)
     {
-        if ((*ptr < *pivot && ptr > pivot) || (*ptr > *pivot && ptr < pivot))
+        if (*ptr < *pivot)
         {
-            std::swap(*ptr, *pivot);
-            pivot = ptr;
+            T tmp = std::move(*pivot);
+            *pivot = std::move(*ptr);
+            *ptr = std::move(pivot[1]);
+            pivot[1] = std::move(tmp);
+            ++pivot;
         }
     }
-    quick_sort(begin, pivot);
-    quick_sort(pivot + 1, end);
+    // Tail recurse with the greater partition
+    if (pivot - begin > end - pivot - 1)
+    {
+        quick_sort(pivot + 1, end);
+        quick_sort(begin, pivot);
+    }
+    else
+    {
+        quick_sort(begin, pivot);
+        quick_sort(pivot + 1, end);
+    }
 }
 
 #endif
