999e0e71192318f5f92edc628046da6b6dab5bbe
Richard Blažek <blazek.risa@gmail.com>
2023-09-19 16:03:11 +0200

    Merge sort

--- main.cpp
+++ main.cpp
@@ -2,22 +2,45 @@
 #include "sort.hpp"
 
 
+template<typename T, size_t S>
+void randomize_array(T (&array)[S])
+{
+    for (size_t i = 0; i < S; ++i)
+    {
+        array[i] = rand();
+    }
+}
+
+template<typename T, size_t S>
+void print_array(T (&array)[S])
+{
+    for (size_t i = 0; i < S; ++i)
+    {
+        std::cout << array[i] << "\n";
+    }
+}
+
+template<typename T, size_t S>
+constexpr size_t array_size(T (&)[S])
+{
+    return S;
+}
+
+
 int main()
 {
     srand(time(0));
 
-    int items[40];
-    for (int& item: items)
-    {
-        item = rand();
-    }
+    int64_t items[55];
 
-    quick_sort(items, items + 40);
-    for (int item: items)
-    {
-        std::cout << item << "\n";
-    }
+    std::cout << "Quick sort:\n";
+    randomize_array(items);
+    quick_sort(items, items + array_size(items));
+    print_array(items);
 
-    std::cout << "\n";
+    std::cout << "\nMerge sort:\n";
+    randomize_array(items);
+    merge_sort(items, array_size(items));
+    print_array(items);
     return 0;
 }
--- sort.hpp
+++ sort.hpp
@@ -8,12 +8,8 @@ using std::size_t;
 
 
 template<typename T>
-void quick_sort(T* begin, T* end)
+T* quick_sort_partition(T* begin, T* end)
 {
-    if (end <= begin + 1)
-    {
-        return;
-    }
     T* pivot = begin + rand() % (end - begin);
     for (T* ptr = begin; ptr < end; ++ptr)
     {
@@ -37,6 +33,17 @@ void quick_sort(T* begin, T* end)
             --pivot;
         }
     }
+    return pivot;
+}
+
+template<typename T>
+void quick_sort(T* begin, T* end)
+{
+    if (end <= begin + 1)
+    {
+        return;
+    }
+    T* pivot = quick_sort_partition(begin, end);
     // Tail recurse with the greater partition
     if (pivot - begin > end - pivot - 1)
     {
@@ -50,8 +57,60 @@ void quick_sort(T* begin, T* end)
     }
 }
 
-/*
+
 template<typename T>
-void merge(T* begin, T* )
-*/
+void merge(T* begin1, T* end1, T* begin2, T* end2, T* dest)
+{
+    while (begin1 != end1 && begin2 != end2)
+    {
+        if (*begin1 <= *begin2)
+        {
+            *dest = std::move(*begin1);
+            ++begin1;
+        }
+        else
+        {
+            *dest = std::move(*begin2);
+            ++begin2;
+        }
+        ++dest;
+    }
+    while (begin1 != end1)
+    {
+        *dest = std::move(*begin1);
+        ++dest;
+        ++begin1;
+    }
+    while (begin2 != end2)
+    {
+        *dest = std::move(*begin2);
+        ++dest;
+        ++begin2;
+    }
+}
+
+template<typename T>
+void merge_sort(T* begin, size_t length)
+{
+    T* buffer = new T[length];
+    T* original = begin;
+    for (size_t partition = 1; partition < length; partition <<= 1)
+    {
+        for (size_t i = 0; i < length; i += partition << 1)
+        {
+            size_t mid_i = std::min(i + partition, length);
+            size_t end_i = std::min(mid_i + partition, length);
+            merge(begin + i, begin + mid_i, begin + mid_i, begin + end_i, buffer + i);
+        }
+
+        std::swap(buffer, begin);
+    }
+
+    if (original == buffer)
+    {
+        std::copy(begin, begin + length, buffer);
+        delete[] begin;
+    }
+}
+
 #endif
