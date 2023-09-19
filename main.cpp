63b41597def91af7e56e9ffb3d48ea84a2999941
Richard Blažek <blazek.risa@gmail.com>
2023-09-19 21:13:52 +0200

    Moved utils to separate file

--- main.cpp
+++ main.cpp
@@ -1,58 +1,7 @@
 #include <iostream>
-#include <type_traits>
+
 #include "sort.hpp"
-
-
-template<typename T, size_t S>
-void randomize_array(T (&array)[S])
-{
-    for (size_t i = 0; i < S; ++i)
-    {
-        array[i] = rand() - RAND_MAX / 2;
-    }
-}
-
-template<size_t S>
-void randomize_array(float (&array)[S])
-{
-    for (size_t i = 0; i < S; ++i)
-    {
-        array[i] = rand() / float(RAND_MAX) * 2 - 1;
-    }
-}
-
-template<typename T, size_t S>
-void print_array(T (&array)[S])
-{
-    for (size_t i = 0; i < S; ++i)
-    {
-        std::cout << array[i] << "\n";
-    }
-}
-
-template<size_t S>
-void print_array(uint8_t (&array)[S])
-{
-    for (size_t i = 0; i < S; ++i)
-    {
-        std::cout << int64_t(array[i]) << "\n";
-    }
-}
-
-template<size_t S>
-void print_array(int8_t (&array)[S])
-{
-    for (size_t i = 0; i < S; ++i)
-    {
-        std::cout << int64_t(array[i]) << "\n";
-    }
-}
-
-template<typename T, size_t S>
-constexpr size_t array_size(T (&)[S])
-{
-    return S;
-}
+#include "array_utils.hpp"
 
 
 template<typename T, size_t S, void (*Sort) (T*, size_t)>
